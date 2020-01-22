d43a042fb2d217c9c73a1a99960a8d55eb5543f6
=nkatz01 <=>
2020-01-22 10:52:10 +0000

    finished q5

--- prolog01Sol.pl
+++ prolog01Sol.pl
@@ -29,12 +29,72 @@ grandson2(X,Y) :- father(Y, Z),
 father(Z,X), \+female(X).
 
 %4
-descendent(X,Y) :- father(Y,X);
-father(Y,Z), descendent(X,Z).
+descendent(X,Y) :- father(Y,X).
+descendent(X,Y) :- father(Y,Z), descendent(X,Z).
 
 commonAncestor(X,Y) :- descendent(X,Z), descendent(Y,Z).
 
-%5 Answer (of which explanation is in attached diagram) follows the naively implemented versions of above clauses.
+%5 Answer (of which explanation is in attached prologTracing.xlsx diagram) follows the naively implemented versions of above clauses.
+?- brother(X,Y).:
+	X = b
+	Y = c
+
+	X = c
+	Y = b
+
+	X = d
+	Y = e
+
+	X = e
+	Y = d
+	
+?- cousin(X,Y).:
+	X = d
+	Y = f
+
+	X = e
+	Y = f
+
+	X = f
+	Y = d
+
+	X = f
+	Y = e
+
+?- grandson(X,Y).:
+	X = d
+	Y = a
+
+	X = e
+	Y = a
+
+	X = f
+	Y = a
+	
+?- descendent(X,Y).:
+	X = b
+	Y = a
+
+	X = c
+	Y = a
+
+	X = d
+	Y = b
+
+	X = e
+	Y = b
+
+	X = f
+	Y = c
+
+	X = d
+	Y = a
+
+	X = e
+	Y = a
+
+	X = f
+	Y = a
 
 
  
